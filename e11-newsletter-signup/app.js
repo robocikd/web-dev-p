dd5a71b47f5f6a6f8dfd3f1f47367991db523e1a
Damian <robocikd@wp.pl>
2022-09-07 20:33:33 +0200

    retrive singup data

--- e11-newsletter-signup/app.js
+++ e11-newsletter-signup/app.js
@@ -5,7 +5,7 @@ const request = require('request');
 const PORT = 3000;
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, function () {
@@ -14,4 +14,11 @@ app.listen(PORT, function () {
 
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/signup.html');
+});
+
+app.post('/', function (req, res) {
+	const firstName = req.body.firstName;
+	const lastName = req.body.lastName;
+	const email = req.body.email;
+	console.log(firstName + lastName + email);
 });
