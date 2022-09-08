aaa278cd2f908d79f5615151dd4f0e621b5dab01
Damian <robocikd@wp.pl>
2022-09-08 18:38:15 +0200

    add success and failure pages

--- e11-newsletter-signup/app.js
+++ e11-newsletter-signup/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const https = require('https');
 
 const PORT = 3000;
 const app = express();
@@ -20,5 +21,42 @@ app.post('/', function (req, res) {
 	const firstName = req.body.firstName;
 	const lastName = req.body.lastName;
 	const email = req.body.email;
-	console.log(firstName + lastName + email);
+
+	const data = {
+		members: [
+			{
+				email_address: email,
+				status: 'subscribed',
+				merge_fields: {
+					FNAME: firstName,
+					LNAME: lastName,
+				},
+			},
+		],
+	};
+	const dataJson = JSON.stringify(data);
+	const url = 'https://us10.api.mailchimp.com/3.0/lists/{yourListId}';
+	const options = {
+		method: 'POST',
+		auth: 'damian:{yourApiKey}',
+	};
+	const request = https.request(url, options, function (response) {
+		const statusCode = response.statusCode;
+
+		if (statusCode === 200) {
+			res.sendFile(`${__dirname}/success.html`);
+		} else {
+			res.sendFile(`${__dirname}/failure.html`);
+		}
+
+		response.on('data', function (data) {
+			console.log(JSON.parse(data));
+		});
+	});
+	request.write(dataJson);
+	request.end();
+});
+
+app.post('/failure', function (req, res) {
+	res.redirect('/');
 });
