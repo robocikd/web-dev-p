64e079fcb3b9af3bfcb97de804a058fecbb1532b
Damian <robocikd@wp.pl>
2022-09-30 13:48:34 +0200

    render a single post on a page

--- e13-EJS-Challenge/app.js
+++ e13-EJS-Challenge/app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const _ = require('lodash');
 
 const homeStartingContent =
 	'Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.';
@@ -39,6 +40,17 @@ app.get('/compose', function (req, res) {
 app.post('/compose', function (req, res) {
 	const post = { postTitle: req.body.postTitle, postBody: req.body.postBody };
 	posts.push(post);
+	res.redirect('/');
+});
+
+app.get('/posts/:postName', function (req, res) {
+	const requestedPostTitle = _.lowerCase(req.params.postName);
+	for (let i = 0; i < posts.length; i++) {
+		const postTitle = _.lowerCase(posts[i].postTitle);
+		if (postTitle === requestedPostTitle) {
+			res.render('post', { postTitle: postTitle, postBody: posts[i].postBody });
+		}
+	}
 	res.redirect('/');
 });
 
